667b4a1b7a6d564f843f922587d7d0421d54047e
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2023-07-17 00:53:53 -0400

    feat: add and adjust say commands

---
+++ exts/say_embeds.py
@@ -0,0 +1,199 @@
+import importlib
+import typing
+
+import interactions as ipy
+import orjson
+
+import common.utils as utils
+
+
+class RawEmbed(utils.Extension):
+    def __init__(self, bot):
+        self.bot: utils.MPBotBase = bot
+        self.name = "Raw Embed"
+        self.add_ext_auto_defer(enabled=False)
+
+    @ipy.slash_command(
+        "say",
+        description="Allows you to send a message as the bot",
+    )
+    @ipy.slash_option(
+        "channel",
+        "The channel to send the message in.",
+        ipy.OptionType.CHANNEL,
+        required=False,
+        channel_types=[ipy.ChannelType.GUILD_TEXT],
+    )
+    async def say(self, ctx: ipy.SlashContext, channel: ipy.GuildText | None = None):
+        if channel is None:
+            channel = ctx.channel  # type: ignore
+            if typing.TYPE_CHECKING:
+                assert channel is not None
+
+        modal = ipy.Modal(
+            ipy.InputText(
+                label="Enter the content you want to send:",
+                style=ipy.TextStyles.PARAGRAPH,
+                custom_id="say-content",
+            ),
+            title="Say Command",
+            custom_id=f"say-cmd|{channel.id}",
+        )
+        await ctx.send_modal(modal)
+
+    @ipy.slash_command(
+        "raw-embed-say",
+        description="Allows you to send an embed from the raw embed JSON format.",
+    )
+    @ipy.slash_option(
+        "channel",
+        "The channel to send the embed in.",
+        ipy.OptionType.CHANNEL,
+        required=False,
+        channel_types=[ipy.ChannelType.GUILD_TEXT],
+    )
+    async def raw_embed_say(
+        self, ctx: ipy.SlashContext, channel: ipy.GuildText | None = None
+    ):
+        if channel is None:
+            channel = ctx.channel  # type: ignore
+            if typing.TYPE_CHECKING:
+                assert channel is not None
+
+        modal = ipy.Modal(
+            ipy.InputText(
+                label="Enter the embed you want to send:",
+                style=ipy.TextStyles.PARAGRAPH,
+                custom_id="embed-say",
+            ),
+            title="Raw Embed Say",
+            custom_id=f"raw-embed-say|{channel.id}",
+        )
+        await ctx.send_modal(modal)
+
+    @ipy.context_menu(
+        "Edit Embed (Raw)",
+        context_type=ipy.CommandType.MESSAGE,
+        default_member_permissions=ipy.Permissions.MANAGE_GUILD,
+        dm_permission=False,
+    )
+    async def raw_embed_edit(self, ctx: ipy.ContextMenuContext):
+        msg: ipy.Message = ctx.target  # type: ignore
+
+        if not msg.embeds:
+            await ctx.send("No embeds found.", ephemeral=True)
+            return
+
+        if msg.author.id != self.bot.user.id:
+            await ctx.send("You can only edit embeds sent by the bot.", ephemeral=True)
+            return
+
+        modal = ipy.Modal(
+            ipy.InputText(
+                label="Enter the embed you want to edit:",
+                style=ipy.TextStyles.PARAGRAPH,
+                value=orjson.dumps(
+                    msg.embeds[0].to_dict(), option=orjson.OPT_INDENT_2
+                ).decode(),
+                custom_id="embed-edit",
+            ),
+            title="Raw Embed Edit",
+            custom_id=f"raw-embed-edit|{msg.id}",
+        )
+        await ctx.send_modal(modal)
+
+    @ipy.context_menu(
+        "Edit Message",
+        context_type=ipy.CommandType.MESSAGE,
+        default_member_permissions=ipy.Permissions.MANAGE_GUILD,
+        dm_permission=False,
+    )
+    async def edit_message(self, ctx: ipy.ContextMenuContext):
+        msg: ipy.Message = ctx.target  # type: ignore
+
+        if msg.author.id != self.bot.user.id:
+            await ctx.send(
+                "You can only edit messages sent by the bot.", ephemeral=True
+            )
+            return
+
+        modal = ipy.Modal(
+            ipy.InputText(
+                label="Enter what you want to edit the message to:",
+                style=ipy.TextStyles.PARAGRAPH,
+                value=msg.content or "N/A",
+                custom_id="edit-content",
+            ),
+            title="Edit Message",
+            custom_id=f"edit-message|{msg.id}",
+        )
+        await ctx.send_modal(modal)
+
+    @ipy.listen(ipy.events.ModalCompletion)  # type: ignore
+    async def on_modal_completion(self, event: ipy.events.ModalCompletion):
+        ctx = event.ctx
+
+        if ctx.custom_id.startswith("raw-embed-say"):
+            channel_id = int(ctx.custom_id.split("|")[1])
+            channel = await self.bot.fetch_channel(channel_id)
+            if not channel:
+                await ctx.send("Could not get channel.", ephemeral=True)
+                return
+
+            try:
+                embed_dict: dict = orjson.loads(ctx.responses["embed-say"])
+            except orjson.JSONDecodeError:
+                await ctx.send("Could not parse the raw embed.", ephemeral=True)
+                return
+
+            if embeds := embed_dict.get("embeds"):
+                embed_dict = embeds[0]
+
+            await ctx.send("Sending...", ephemeral=True)
+            await channel.send(embed=embed_dict)
+
+        elif ctx.custom_id.startswith("raw-embed-edit"):
+            msg_id = int(ctx.custom_id.split("|")[1])
+
+            try:
+                embed_dict: dict = orjson.loads(ctx.responses["embed-edit"])
+            except orjson.JSONDecodeError:
+                await ctx.send("Could not parse the raw embed.", ephemeral=True)
+                return
+
+            if embeds := embed_dict.get("embeds"):
+                embed_dict = embeds[0]
+
+            await ctx.send("Editing...", ephemeral=True)
+
+            message = await ctx.channel.fetch_message(msg_id)
+            if message:
+                await message.edit(embed=embed_dict)
+            else:
+                await ctx.send("Could not get message.", ephemeral=True)
+                return
+
+        elif ctx.custom_id.startswith("say-cmd"):
+            channel_id = int(ctx.custom_id.split("|")[1])
+            channel = await self.bot.fetch_channel(channel_id)
+            if not channel:
+                await ctx.send("Could not get channel.", ephemeral=True)
+                return
+
+            await ctx.send("Sending...", ephemeral=True)
+            await channel.send(content=ctx.responses["say-content"])
+
+        elif ctx.custom_id.startswith("edit-message"):
+            msg_id = int(ctx.custom_id.split("|")[1])
+            message = await ctx.channel.fetch_message(msg_id)
+            if not message:
+                await ctx.send("Could not get message.", ephemeral=True)
+                return
+
+            await ctx.send("Editing...", ephemeral=True)
+            await message.edit(content=ctx.responses["edit-content"])
+
+
+def setup(bot):
+    importlib.reload(utils)
+    RawEmbed(bot)
