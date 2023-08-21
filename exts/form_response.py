18333a1f07e8737e3c3a316edd8195e3749c216e
AstreaTSS <25420078+AstreaTSS@users.noreply.github.com>
2023-08-20 21:39:10 -0400

    feat: add art form response

--- exts/form_response.py
+++ exts/form_response.py
@@ -41,6 +41,7 @@ class FormResponse(ipy.Extension):
 
         app = web.Application()
         app.add_routes([web.post("/mp-form-response", self.form_response)])
+        app.add_routes([web.post("/mp-art-form-response", self.art_form_response)])
         self.runner = web.AppRunner(app)
         await self.runner.setup()
         site = web.TCPSite(self.runner, port=8080)
@@ -107,6 +108,67 @@ class FormResponse(ipy.Extension):
 
         await self.staff_chat.send(embeds=embed)
         await self.bot.redis.set(f"mp-form:{form_data.id}", "1")
+
+    async def art_form_response(
+        self,
+        request: web.Request,
+    ):
+        try:
+            resp_json = await request.json(loads=orjson.loads)
+        except orjson.JSONDecodeError:
+            return web.Response(status=400)
+
+        if not resp_json.get("kinda_secret"):
+            return web.Response(status=404)
+
+        if resp_json["kinda_secret"] != os.environ["KINDA_FORMS_SECRET"]:
+            return web.Response(status=404)
+
+        asyncio.create_task(self.actual_art_response(resp_json))
+        return web.Response(status=200)
+
+    async def actual_art_response(self, resp_json: dict[str, typing.Any]):
+        resp_json.pop("kinda_secret")
+        form_data = FormData(**resp_json)
+
+        if await self.bot.redis.get(f"mp-form:{form_data.id}"):
+            return
+
+        user_question = next(i for i in form_data.items if i["id"] == 313318916)
+        username = user_question["answer"]
+
+        username_to_search = username
+        hash_split = username.split("#")
+        discrim = "0"
+
+        if len(hash_split) == 2 and hash_split[1].isdigit() and len(hash_split[1]) == 4:
+            username_to_search = hash_split[0]
+            discrim = hash_split[1]
+
+        if possible_member := next(
+            (
+                m
+                for m in self.bot.guild.members
+                if m.username == username_to_search and m.discriminator == discrim
+            ),
+            None,
+        ):
+            username = possible_member.mention
+
+        embed = ipy.Embed(
+            title="Artist Form Response",
+            color=self.bot.color,
+            timestamp=ipy.Timestamp.utcnow(),
+        )
+        embed.add_field("Submitted User", username, inline=True)
+        embed.add_field(
+            "Submitted Form (As Pre-Filled Template)",
+            f"[Link]({form_data.url})",
+            inline=True,
+        )
+
+        await self.staff_chat.send(embeds=embed)
+        await self.bot.redis.set(f"mp-art-form:{form_data.id}", "1")
 
 
 def setup(bot: utils.MPBotBase) -> None:
